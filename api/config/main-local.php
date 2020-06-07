0d5ba887de18d938d5247248d95237bed8f8ef29
xqf <xuquanfu_2004@163.com>
2020-06-07 15:05:12 +0800

    修改配置

---
+++ api/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'PxouE4uZnrCLNZwF97v2wWvfSMzTW2g6',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ common/config/params-local.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'okex_config' => [
+        'apiKey' => '',
+        'apiSecret' => '',
+        'passphrase' => '',
+    ],
+    'coin' => [
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+        'BTC',
+    ]
+];
